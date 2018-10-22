9be3b674df3072c839409650aaf7cd180cee09e1
Greg Stoll <greg@gregstoll.com>
2018-10-22 16:15:42 -0500

    Add an example node that calls a .NET method at runtime (#9)
    * Add an example node that calls a .NET method at runtime
    
    * Improve documentation on example nodes
    
    Improve documentation on example nodes, including getting rid of  <inheritdoc/>s

---
+++ ExamplePlugins/ExampleNode/Design/CalculateTotalLengthViewModel.cs
@@ -0,0 +1,38 @@
+﻿using NationalInstruments.Core;
+using NationalInstruments.Design;
+using NationalInstruments.SourceModel;
+
+namespace ExamplePlugins.ExampleNode.Design
+{
+    /// <summary>
+    /// The ViewModel for the Calculate Total Length Node
+    /// The ViewModel controls the interaction with the user
+    /// </summary>
+    public class CalculateTotalLengthViewModel : NodeViewModel
+    {
+        /// <summary>
+        /// Constructs a new instance
+        /// </summary>
+        /// <param name="model">The model element we are bound to</param>
+        public CalculateTotalLengthViewModel(Node model) :
+            base(model)
+        {            
+        }
+
+        /// <summary>
+        /// Returns the Uri (resource location) for our node foreground image
+        /// </summary>
+        protected override ResourceUri ForegroundUri
+        {
+            get { return new ResourceUri(GetType(), "Resources/Llama"); }
+        }
+
+        /// <summary>
+        /// Returns the render data for our node's foreground image
+        /// </summary>
+        public override NineGridData ForegroundImageData
+        {
+            get { return new ViewModelIconData(this) { ImageUri = ForegroundUri }; }
+        }
+    }
+}
