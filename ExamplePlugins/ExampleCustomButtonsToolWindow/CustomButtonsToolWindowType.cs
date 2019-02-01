b9c208caff16d6d27d1aa1bf4aefb3a6d41e3460
shastriUF <aniruddha.shastri@gmail.com>
2019-02-01 15:11:18 -0600

    Update the CustomButtonsToolWindow example so that it builds

--- ExamplePlugins/ExampleCustomButtonsToolWindow/CustomButtonsToolWindowType.cs
+++ ExamplePlugins/ExampleCustomButtonsToolWindow/CustomButtonsToolWindowType.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 using NationalInstruments.Composition;
 using NationalInstruments.Shell;
 
@@ -8,33 +7,50 @@ namespace ExamplePlugins.ExampleCustomButtonsToolWindow
     /// <summary>
     /// Control type for the Custom Buttons window
     /// </summary>
+    /// <remarks>
+    /// The "Name" defines the title of the tool. The framework will use the "DisplayName" resource from a resx file with the same name.
+    /// For example, this example tool will use the name defined in Resources/CustomButtonsToolWindow.resx.
+    /// 
+    /// The "Weight" defines where the tool appears in the list. Weights from LabVIEWFamilyApplicationFeatureSet.CreateApplicationContent
+    ///     Output .1
+    ///     Errors .2
+    ///     Build Queue .21
+    ///     Separator .25
+    ///     C .3
+    ///     Separator .35
+    ///     Algorithm Estimates .4
+    ///     Convert to fixed point .41
+    ///     Sampling probes .42
+    ///     Timing violations .43
+    ///     Separator .5
+    ///     Schedule .6
+    /// </remarks>
     [Export(typeof(IToolWindowType))]
     [ExportMetadata("UniqueID", WindowGuidText)]
-    [Name("(Example) Custom Buttons")]
+    [Name(typeof(CustomButtonsToolWindow))]
     [ExportMetadata("SmallImagePath", "")]
     [ExportMetadata("LargeImagePath", "")]
     [ExportMetadata("DefaultCreationTime", ToolWindowCreationTime.UserRequested)]
     [ExportMetadata("DefaultCreationMode", ToolWindowCreationMode.Pinned)]
     [ExportMetadata("DefaultLocation", ToolWindowLocation.Bottom)]
     [ExportMetadata("AssociatedDocumentType", "")]
-    [ExportMetadata("Weight", 0.5)]
+    [ExportMetadata("Weight", 0.8)]
     [ExportMetadata("ForceOpenPinned", true)]
     [ExportMetadata("VisibleInApplicationMenu", VisibilityOption.ToolLauncher)]
-    internal class CustomButtonsToolWindowType : ToolWindowType
+    public class CustomButtonsToolWindowType : ToolWindowType
     {
-        private const string WindowGuidText = "{29487F0A-2E72-4A1E-BFD6-5A42B37655E8}";
-
         /// <summary>
         /// The Custom Buttons window GUID.
         /// </summary>
         /// <remarks>
         /// DO NOT COPY THE GUID IF YOU ARE COPYING THIS CODE! CREATE A NEW GUID AND USE THAT INSTEAD.
         /// </remarks>
-        internal static readonly Guid WindowGuid = new Guid(WindowGuidText);
+        private const string WindowGuidText = "{29487F0A-2E72-4A1E-BFD6-5A42B37655E8}";
 
         /// <inheritdoc />
         public override IToolWindowViewModel CreateToolWindow(ToolWindowEditSite editSite)
         {
+            // Use CreateInstance to create the CustomButtonsToolWindowViewModel so that the Host is imported correctly.
             var viewModel = Host.CreateInstance<CustomButtonsToolWindowViewModel>();
             viewModel.EditSite = editSite;
             return viewModel;
